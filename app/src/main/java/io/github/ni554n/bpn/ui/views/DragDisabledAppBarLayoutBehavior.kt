1de274d783eb3de58d6e8321970ddf0e6b9cf2d9
Nissan Ahmed <aan007bd@gmail.com>
2022-07-25 19:27:11 +0600

    Refactor

--- app/src/main/java/io/github/ni554n/bpn/ui/views/DragDisabledAppBarLayoutBehavior.kt
+++ app/src/main/java/io/github/ni554n/bpn/ui/views/DragDisabledAppBarLayoutBehavior.kt
@@ -4,12 +4,8 @@ import android.content.Context
 import android.util.AttributeSet
 import com.google.android.material.appbar.AppBarLayout
 
-// Not unused. "app:layout_behavior" uses class by its full class name from XML.
-@Suppress("unused")
-class DragDisabledAppBarLayoutBehavior : AppBarLayout.Behavior {
-
-  constructor() : super()
-  constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+class DragDisabledAppBarLayoutBehavior(context: Context? = null, attrs: AttributeSet? = null) :
+  AppBarLayout.Behavior(context, attrs) {
 
   init {
     setDragCallback(object : DragCallback() {
