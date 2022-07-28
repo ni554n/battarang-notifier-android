6907124288b89bc6ba152b707f1abce6085589a8
Nissan Ahmed <aan007bd@gmail.com>
2022-07-28 16:48:47 +0600

    Co-locate extension functions

--- app/src/main/kotlin/io/github/ni554n/bpn/event/BroadcastReceiverRegistererService.kt
+++ app/src/main/kotlin/io/github/ni554n/bpn/event/BroadcastReceiverRegistererService.kt
@@ -1,17 +1,20 @@
 package io.github.ni554n.bpn.event
 
-import android.app.Service
+import android.app.*
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.BatteryManager
 import android.os.Build
 import android.os.IBinder
+import android.provider.Settings
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import io.github.ni554n.bpn.BootEventReceiver.Companion.resumeAfterBoot
+import io.github.ni554n.bpn.MainActivity
 import io.github.ni554n.bpn.event.receivers.AlarmBroadcastReceivers
 import io.github.ni554n.bpn.event.receivers.PowerBroadcastReceivers
-import io.github.ni554n.bpn.ui.buildServiceNotification
 import logcat.logcat
 import org.koin.android.ext.android.inject
 
@@ -98,4 +101,61 @@ class BroadcastReceiverRegistererService : Service() {
       logcat { "Stopped the foreground service" }
     }
   }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+private fun Context.buildServiceNotification(): Notification {
+  val batteryStateChannelId = createBatteryStateChannel()
+
+  val notificationTapActionPendingIntent: PendingIntent = PendingIntent.getActivity(
+    this,
+    256,
+    Intent(this, MainActivity::class.java),
+    PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT,
+  )
+
+  val minimizeButtonActionPendingIntent: PendingIntent = PendingIntent.getActivity(
+    this,
+    512,
+    Intent(Settings.ACTION_CHANNEL_NOTIFICATION_SETTINGS).apply {
+      putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
+      putExtra(Settings.EXTRA_CHANNEL_ID, batteryStateChannelId)
+    },
+    PendingIntent.FLAG_IMMUTABLE,
+  )
+
+  return NotificationCompat.Builder(this, batteryStateChannelId)
+    .setContentTitle("Listening for battery related events idly") // TODO Extract these to string resource
+    .setContentText("You can tap the Minimize button to open the channel settings and tap turn on Minimize to this service notification in the silent section. May need to toggle the Notification Service for it to minimize.")
+    .setSmallIcon(android.R.drawable.ic_lock_idle_low_battery)
+    .setTicker("Battery State Ticker") // Remove it if the UX feels better
+    .setPriority(NotificationCompat.PRIORITY_MIN)
+    .setForegroundServiceBehavior(NotificationCompat.FOREGROUND_SERVICE_IMMEDIATE)
+    .setContentIntent(notificationTapActionPendingIntent)
+    .addAction(
+      android.R.drawable.ic_lock_idle_low_battery,
+      "Minimize this Notification",
+      minimizeButtonActionPendingIntent,
+    ).build()
+}
+
+// Notification channel will not be created multiple times
+@RequiresApi(Build.VERSION_CODES.O)
+private fun Context.createBatteryStateChannel(): String {
+  val channelId = "BATTERY_STATE"
+
+  val batteryStateChannel: NotificationChannel = NotificationChannel(
+    channelId,
+    "Battery Event Listener",
+    NotificationManager.IMPORTANCE_MIN,
+  ).apply {
+    description = "Listens for battery states"
+    setShowBadge(false)
+  }
+
+  (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager).createNotificationChannel(
+    batteryStateChannel
+  )
+
+  return channelId
 }
